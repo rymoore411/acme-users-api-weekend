b6ee4766061cef5e43f33c3615ec1f40b678a850
Ryan Moore <rymoore411@gmail.com>
2019-07-01 21:59:28 -0700

    lost

--- models.js
+++ models.js
@@ -43,11 +43,17 @@ Department.hasMany(User);
 
 
 const syncAndSeed = async ()=> {
+
+  try{
   await conn.sync({ force: true});
   const departments = ['HR', 'Engineering', 'Sales'];
   const [hr, engineering, sales] = await Promise.all(departments.map(dept => Department.create({dept})));
   const users = ['Ryan', 'Jason', 'Matt'];
   const [ryan, jason, matt] = await Promise.all(users.map(user => User.create({user})));
+  }
+  catch(err){
+    console.log(err);
+  }
 }
 
 syncAndSeed();
