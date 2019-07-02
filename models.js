e77ac049d3c799f4c479764ffa073525d210c45a
Ryan Moore <rymoore411@gmail.com>
2019-07-01 21:42:59 -0700

    got some issues

--- models.js
+++ models.js
@@ -13,10 +13,41 @@ const User = conn.define('user', {
   },
 
   id: {
-    type: Sequelize.UUIDV4,
-    primaryKey: true
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4
   }
 
 })
 
+const Department = conn.define('department', {
 
+  name:{
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  id: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4
+  }
+
+})
+
+User.belongsTo(Department);
+Department.hasMany(User);
+
+
+const syncAndSeed = async ()=> {
+  await conn.sync({ force: true});
+  const departments = ['HR', 'Engineering', 'Sales'];
+  const [hr, engineering, sales] = await Promise.all(departments.map(dept => Department.create({dept})));
+  const users = ['Ryan', 'Jason', 'Matt'];
+  const [ryan, jason, matt] = await Promise.all(users.map(user => User.create({user})));
+}
+
+syncAndSeed();
